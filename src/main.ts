201b0442667c065cef7c948acb67a0c1bc47a5e0
nu5rim3 <nusrisamath@gmail.com>
2024-07-15 18:23:35 +0530

    FEAT: updated with swagger

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { WinstonModule } from 'nest-winston';
 import * as dotenv from 'dotenv';
 import { winstonLogger } from './logger/logger.service';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 dotenv.config();
 
@@ -12,6 +13,30 @@ async function bootstrap() {
     logger: WinstonModule.createLogger({
       instance: winstonLogger,
     }),
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('BookWorm API')
+    .setDescription('The BookWorm API.')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document, {
+    customSiteTitle: 'Swagger UI - BookWorm API',
+    customfavIcon:
+      'https://www.openpricer.com/wp-content/uploads/2020/03/logo-swagger-api.png',
+    customJs: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.17.14/swagger-ui.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.17.14/swagger-ui-bundle.min.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.17.14/swagger-ui-bundle.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.17.14/swagger-ui-standalone-preset.min.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.17.14/swagger-ui-standalone-preset.js',
+    ],
+    customCssUrl: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.17.14/swagger-ui.min.css',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.17.14/swagger-ui.css',
+    ],
   });
 
   const port = process.env.PORT || 3000;
